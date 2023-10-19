9024cd7de00b417c878ecbb7070ebd4048fd0afa
Evan Losh <evand.losh@gmail.com>
2023-10-19 13:13:14 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Write your code here!
+
+// test 1 is passing
+const main = document.getElementById("main");
+main.parentNode.removeChild(main);
+
+//test 2 is passing
+const newHeader = document.createElement("h1");
+document.body.append(newHeader);
+
+//test 3 is failing
+newHeader.id = "victory";
+
+//test 4 is failing
+text = document.createTextNode("Danny Devito is the champion");
+newHeader.appendChild(text);
